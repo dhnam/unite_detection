c6c47ecd90878751abea09944655a52ba878cd39
LegenDUST <dhnam0502@naver.com>
2026-02-14 08:43:50 +0000

    edit gpu dependency

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,11 @@
+#! /bin/bash
 pip install uv
 sudo apt install -y libgl1
-uv sync
+
+if command -v nvidia-smi &> /dev/null; then
+    echo "GPU detected. Installing Torch with CUDA 12.6..."
+    uv sync --extra gpu
+else
+    echo "No GPU detected. Installing Torch CPU version..."
+    uv sync --extra cpu
+fi
